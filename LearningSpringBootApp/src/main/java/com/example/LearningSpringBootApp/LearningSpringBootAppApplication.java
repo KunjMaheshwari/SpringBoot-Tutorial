48e6fec2ec498a5402d98ce556b8616acd773f66
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2025-08-17 18:24:39 +0530

    Added Part 1

---
+++ LearningSpringBootApp/src/main/java/com/example/LearningSpringBootApp/LearningSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.LearningSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootAppApplication.class, args);
+	}
+}
